4f584647469c0b971ca5fcba73525270a9557eee
HansenLYX0708 <1195857096@qq.com>
2018-04-04 14:37:43 +0800

    Initial commit

---
+++ HanSL.Log/Log/ILogFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HanSL.Log.Log
+{
+    public interface ILogFactory
+    {
+        ILog GetLog(string name);
+    }
+}
---
+++ LogTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HanSL.Log.Log;
+
+namespace LogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //日志HanSL的Debug信息
+            new HanSL.Log.Log.LogFactory().GetLog("HanSL").Debug(true, "Hello");
+            new HanSL.Log.Log.LogFactory().GetLog("HanSL").Debug(true, "World");
+
+            //日志HanSl的info信息
+            new HanSL.Log.Log.LogFactory().GetLog("HanSL").Info(true, "Hello");
+            new HanSL.Log.Log.LogFactory().GetLog("HanSL").Info(true, "HanSL");
+            Console.Read();
+        }
+    }
+}
